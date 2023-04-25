b1618c11225d6e3c0025c32d610ed8414fd2eae6
Daniel Gooden <dan.gooden.dev@gmail.com>
2023-04-25 20:26:41 +1000

    creating addBookToLibrary function

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, readStatus) {
   this.title = title;
   this.author = author;
@@ -9,5 +11,15 @@ Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}`;
 };
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-console.log(theHobbit.info());
+function addBookToLibrary(book) {
+  if (typeof book === 'object') {
+    const newBook = new Book(book.title, book.author, book.pages, book.readStatus);
+    myLibrary.push(newBook);
+  }
+}
+
+// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+// addBookToLibrary(theHobbit);
+
+// console.log(myLibrary);
+

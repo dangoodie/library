d4f20198a7e2f89c4acf80bc2ebd2307db2c369a
Daniel Gooden <dan.gooden.dev@gmail.com>
2023-04-25 22:40:43 +1000

    fixing card styling for the width

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const library = document.querySelector(".library");
 
 function Book(title, author, pages, readStatus) {
   this.title = title;
@@ -12,8 +13,13 @@ Book.prototype.info = function () {
 };
 
 function addBookToLibrary(book) {
-  if (typeof book === 'object') {
-    const newBook = new Book(book.title, book.author, book.pages, book.readStatus);
+  if (typeof book === "object") {
+    const newBook = new Book(
+      book.title,
+      book.author,
+      book.pages,
+      book.readStatus
+    );
     myLibrary.push(newBook);
   }
 }
@@ -22,3 +28,42 @@ const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
 addBookToLibrary(theHobbit);
 
 // console.log(myLibrary);
+
+function buildCard(book) {
+  // build card div
+  const card = document.createElement("div");
+  card.classList.add("card");
+  library.appendChild(card);
+
+  // build individual elements in the card
+  const title = document.createElement("p");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const readStatus = document.createElement("div");
+
+  // add classes
+  title.classList.add("title");
+  author.classList.add("author");
+  pages.classList.add("pages");
+  readStatus.classList.add("read-status");
+
+  // adding text
+  title.textContent = book.title;
+  author.textContent = book.author;
+  pages.textContent = book.pages;
+  readStatus.textContent = book.readStatus;
+
+  // append children
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(readStatus);
+}
+
+function renderLibrary (books) {
+  books.forEach(book => {
+    buildCard(book);
+  });
+}
+
+renderLibrary(myLibrary);

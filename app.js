40327f6d8904960f6ee19ab8e366d5b7c465043c
Daniel Gooden <dan.gooden.dev@gmail.com>
2023-04-25 19:50:55 +1000

    copying the book constructor to this repo

---
+++ app.js
@@ -0,0 +1,13 @@
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+}
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}`;
+};
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+console.log(theHobbit.info());
